81610e3c3d703cd409637701bd3bb053055f90eb
Paolo Furini <p.furini@nexbit.it>
2016-07-12 00:19:59 +0200

    feat(alpine): rename alpine version to openresty-alpine

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --rm=true --force-rm -t nexbit/openresty .
-docker build --rm=true --force-rm -t nexbit/openresty:onbuild ./onbuild
+docker build --rm=true --force-rm -t nexbit/openresty-alpine .
+docker build --rm=true --force-rm -t nexbit/openresty-alpine:onbuild ./onbuild
