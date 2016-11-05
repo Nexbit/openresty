d14f005db16e72a51eca53c2f2838a7c78a15635
Paolo Furini <paolo.furini@gmail.com>
2016-11-05 11:45:53 +0100

    chore(project): Debian build script updated

--- debian/build.sh
+++ debian/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --rm=true --force-rm -t nexbit/openresty .
+docker build --rm=true --force-rm -t nexbit/openresty . && \
 docker build --rm=true --force-rm -t nexbit/openresty:onbuild ./onbuild
